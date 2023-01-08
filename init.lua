8f7c5cfde533ed6d7580f326d5b1fe0a04fa0b78
rageofgods <rageofgods@gmail.com>
2023-01-08 12:34:49 +0300

    feat: additional options configure

--- init.lua
+++ init.lua
@@ -47,6 +47,14 @@ local config = {
       spell = false, -- sets vim.opt.spell
       signcolumn = "auto", -- sets vim.opt.signcolumn to auto
       wrap = false, -- sets vim.opt.wrap
+      conceallevel = 2, -- enable conceal
+      foldenable = false,
+      foldexpr = "nvim_treesitter#foldexpr()", -- set Treesitter based folding
+      foldmethod = "expr",
+      linebreak = true, -- linebreak soft wrap at words
+      list = true, -- show whitespace characters
+      listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
+      showbreak = "↪ ",
     },
     g = {
       mapleader = " ", -- sets vim.g.mapleader
